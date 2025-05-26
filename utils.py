f34b64177da14cc83b680173a86310bb60397c1e
Walid Sabhi <101206416+flooki10@users.noreply.github.com>
2025-05-26 12:14:59 +0200

    Delete utils.py

--- utils.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-def validate_date(date_str: str) -> datetime:
-    try:
-        return datetime.fromisoformat(date_str)
-    except ValueError:
-        return None
